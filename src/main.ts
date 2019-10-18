4ad0a64e1f3bce6265cb860abb6b3be114da3d37
Helias <stefanoborzi32@gmail.com>
2019-10-19 01:01:00 +0200

    Enabled CORS, improved characters /online route

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap()
     const logger = new Logger('bootstrap');
     const app = await NestFactory.create(AppModule);
     const port = process.env.WEBSITE_PORT || 3000;
-
+	app.enableCors();
     await app.listen(port);
     logger.log(`Application listening on port ${port}`);
 }
