00b5a4d931fb999e548773e4e3f4cc5eb0513705
Helias <stefanoborzi32@gmail.com>
2019-10-18 12:56:32 +0200

    Fixed path for Windows

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as path from 'path';
 
 export const AuthDatabaseConfig: TypeOrmModuleOptions =
 {
@@ -8,7 +9,7 @@ export const AuthDatabaseConfig: TypeOrmModuleOptions =
     username: process.env.AUTH_DATABASE_USERNAME,
     password: process.env.AUTH_DATABASE_PASSWORD,
     database: process.env.AUTH_DATABASE_NAME,
-    entities: [__dirname + '/../auth/*.entity.{js, ts}'],
+    entities: [path.join(__dirname, '..', 'auth', '*.entity.{js, ts}')],
     synchronize: true
 };
 
@@ -20,7 +21,7 @@ export const WorldDatabaseConfig: TypeOrmModuleOptions =
     username: process.env.WORLD_DATABASE_USERNAME,
     password: process.env.WORLD_DATABASE_PASSWORD,
     database: process.env.WORLD_DATABASE_NAME,
-    entities: [__dirname + '/../world/*.entity.{js, ts}'],
+    entities: [path.join(__dirname, '..', 'world', '*.entity.{js, ts}')],
     synchronize: true
 };
 
@@ -32,7 +33,7 @@ export const CharactersDatabaseConfig: TypeOrmModuleOptions =
     username: process.env.CHARACTERS_DATABASE_USERNAME,
     password: process.env.CHARACTERS_DATABASE_PASSWORD,
     database: process.env.CHARACTERS_DATABASE_NAME,
-    entities: [__dirname + '/../characters/*.entity.{js, ts}'],
+    entities: [path.join(__dirname, '..', 'characters', '*.entity.{js, ts}')],
     synchronize: true
 };
 
@@ -45,6 +46,6 @@ export const WebsiteDatabaseConfig: TypeOrmModuleOptions =
     username: process.env.WEB_SITE_DATABASE_USERNAME,
     password: process.env.WEB_SITE_DATABASE_PASSWORD,
     database: process.env.WEB_SITE_DATABASE_NAME,
-    entities: [__dirname + '/../website/*.entity.{js, ts}'],
+    entities: [path.join(__dirname, '..', 'website', '*.entity.{js, ts}')],
     synchronize: true
 };
