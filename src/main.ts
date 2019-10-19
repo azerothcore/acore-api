383269e16782106e4af158c04afb72e41f40b16d
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2019-10-19 18:08:03 +0330

    Helmet, RateLimit, Compression

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,22 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import * as compression from 'compression';
 
 async function bootstrap()
 {
     const logger = new Logger('bootstrap');
     const app = await NestFactory.create(AppModule);
     const port = process.env.WEBSITE_PORT || 3000;
+
     app.enableCors();
+    app.use(helmet());
+    app.use(rateLimit({ max: 100, windowMs: 60 * 60 * 1000, message: 'Too many requests from this IP, Please try again in an hour!' }));
+    app.use(compression());
     await app.listen(port);
+
     logger.log(`Application listening on port ${port}`);
 }
 
