2f4b38417307666b23cd00cd8b956624674f47f0
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2019-12-17 23:11:53 +0330

    Added e2e test for SignUp && SignIn

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,27 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import 'dotenv/config';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AccountDto } from '../src/auth/dto/account.dto';
+import { HttpStatus } from '@nestjs/common';
 
-describe('AppController (e2e)', () =>
+const app = 'http://localhost:3000';
+
+describe('Auth', () =>
 {
-    let app;
-
-    beforeEach(async () =>
+    const account: AccountDto =
     {
-        const moduleFixture: TestingModule = await Test.createTestingModule(
-        {
-            imports: [AppModule],
-        }).compile();
+        username: 'AzerothJS',
+        firstName: 'Azeroth',
+        lastName: 'API',
+        phone: '+989171111111',
+        password: 'Azer0thjs!@',
+        passwordConfirm: 'Azer0thjs!@',
+        email: 'Azeroth@Example.com'
+    };
 
-        app = moduleFixture.createNestApplication();
-        await app.init();
+    it('Should SignUp Account', () =>
+    {
+        return request(app)
+            .post('/auth/signup')
+            .set('Accept', 'application/json')
+            .send(account)
+            .expect(HttpStatus.CREATED);
     });
 
-    it('/ (GET)', () =>
+    it('Should Reject Duplicate Account, Email, Phone', () =>
     {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!');
+        return request(app)
+            .post('/auth/signup')
+            .set('Accept', 'application/json')
+            .send(account)
+            .expect(HttpStatus.CONFLICT);
+    });
+
+    it('Should SignIn Account', () =>
+    {
+        return request(app)
+            .post('/auth/signin')
+            .set('Accept', 'application/json')
+            .send({ username: account.username, password: account.password })
+            .expect(HttpStatus.OK);
     });
 });
